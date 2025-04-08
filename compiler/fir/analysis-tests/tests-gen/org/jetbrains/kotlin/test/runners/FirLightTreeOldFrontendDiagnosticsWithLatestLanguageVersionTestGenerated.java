4e1fecdcd9cc5394e059ff7a033515e5f0c52d13
Denis.Zharkov <denis.zharkov@jetbrains.com>
2025-04-08 13:49:01 +0200

    Add test for KT-74932

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGenerated.java
@@ -39833,6 +39833,12 @@ public class FirLightTreeOldFrontendDiagnosticsWithLatestLanguageVersionTestGene
       }
 
       @Test
+      @TestMetadata("complexCases.kt")
+      public void testComplexCases() {
+        runTest("compiler/testData/diagnostics/tests/suspendConversion/complexCases.kt");
+      }
+
+      @Test
       @TestMetadata("conversionInLastStatementOfWhenBranch.kt")
       public void testConversionInLastStatementOfWhenBranch() {
         runTest("compiler/testData/diagnostics/tests/suspendConversion/conversionInLastStatementOfWhenBranch.kt");
