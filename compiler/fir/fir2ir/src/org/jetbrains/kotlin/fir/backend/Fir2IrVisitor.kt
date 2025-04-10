942de3ef2d077993c379ce400929316b5f1f571b
Alexander Udalov <alexander.udalov@jetbrains.com>
2025-04-10 11:31:18 +0200

    Fir2Ir: fix start offsets of functions and constructors
    Also, change the offsets of the delegating constructor call and the
    instance initializer of the primary constructor to be those of the
    class, not of the constructor, as in K1. Otherwise, line number on the
    class would not be generated, so the debugger wouldn't stop at the
    "class" keyword.
    
     #KT-64731 Fixed
     #KT-74569 Fixed

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -163,7 +163,9 @@ class Fir2IrVisitor(
             if (delegatedConstructor != null) {
                 with(memberGenerator) {
                     irEnumEntry.initializerExpression = IrFactoryImpl.createExpressionBody(
-                        delegatedConstructor.toIrDelegatingConstructorCall()
+                        delegatedConstructor.convertWithOffsets { startOffset, endOffset ->
+                            delegatedConstructor.toIrDelegatingConstructorCall(startOffset, endOffset)
+                        }
                     )
                 }
             }
