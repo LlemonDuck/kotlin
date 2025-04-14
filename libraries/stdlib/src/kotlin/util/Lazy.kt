643558fbfbcbc1b337db26dfd9b84df4f5959057
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-04-14 12:43:52 -0400

    Improve deserialization implementation for classes using proxy pattern
    Override readObject method for classes relying on
    Java Serialization's serialization proxy pattern to
    throw an exception on an attempt to bypass the proxy.
    
    ^KT-76321

--- libraries/stdlib/src/kotlin/util/Lazy.kt
+++ libraries/stdlib/src/kotlin/util/Lazy.kt
@@ -8,6 +8,8 @@
 
 package kotlin
 
+import kotlin.internal.ReadObjectInput
+import kotlin.internal.throwReadObjectNotSupported
 import kotlin.reflect.KProperty
 
 /**
@@ -107,6 +109,8 @@ internal class UnsafeLazyImpl<out T>(initializer: () -> T) : Lazy<T>, Serializab
     override fun toString(): String = if (isInitialized()) value.toString() else "Lazy value not initialized yet."
 
     private fun writeReplace(): Any = InitializedLazyImpl(value)
+
+    private fun readObject(input: ReadObjectInput): Unit = throwReadObjectNotSupported()
 }
 
 internal class InitializedLazyImpl<out T>(override val value: T) : Lazy<T>, Serializable {
