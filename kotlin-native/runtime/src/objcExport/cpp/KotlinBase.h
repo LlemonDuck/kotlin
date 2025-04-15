1a80096f8af972fee28c5a76f9da70dbb7eccf0e
Gleb Lukianets <gleb.lukianets@jetbrains.com>
2025-04-15 16:21:57 +0200

    Forbid +new

--- kotlin-native/runtime/src/objcExport/cpp/KotlinBase.h
+++ kotlin-native/runtime/src/objcExport/cpp/KotlinBase.h
@@ -26,6 +26,7 @@ struct ObjHeader;
 
 + (KotlinBase *)createExistentialWrappingExternalRCRef:(void *)ref NS_SWIFT_NAME(__createExistential(externalRCRef:));
 
++ (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
 
 - (instancetype)initWithExternalRCRefUnsafe:(void *)ref cache:(BOOL)shouldCache substitute:(BOOL)shouldSubstitute NS_DESIGNATED_INITIALIZER NS_REFINED_FOR_SWIFT;
